0f098b9d2a22a8f82b80eede8985db25383ceffc
robert.lee <robert.lee@iisigroup.com>
2022-04-28 18:49:18 +0800

    add test code inchpater 2

--- chapter-02/hello-streams/src/test/java/com/example/ProcessorApiExampleTest.java
+++ chapter-02/hello-streams/src/test/java/com/example/ProcessorApiExampleTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.TestInputTopic;
 import org.apache.kafka.streams.Topology;
@@ -14,40 +13,43 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ProcessorApiExampleTest {
-	private TopologyTestDriver testDriver;
-	private TestInputTopic<Void, String> inputTopic;
+  private TopologyTestDriver testDriver;
+  private TestInputTopic<Void, String> inputTopic;
 
-	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-	private final PrintStream originalOut = System.out;
-	private final PrintStream originalErr = System.err;
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+  private final PrintStream originalErr = System.err;
 
-	@BeforeEach
-	protected void setUp() throws Exception {
-		System.setOut(new PrintStream(outContent));
-		System.setErr(new PrintStream(errContent));
+  @BeforeEach
+  protected void setUp() throws Exception {
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
 
-		Topology topology = new Topology();
-		topology.addSource("UserSource", "users");
-		topology.addProcessor("SayHello", SayHelloProcessor::new, "UserSource");
+    Topology topology = new Topology();
+    topology.addSource("UserSource", "users");
+    topology.addProcessor("SayHello", SayHelloProcessor::new, "UserSource");
 
-		// create a test driver. we will use this to pipe data to our topology
-		testDriver = Utils.createTestDriverAndTestTopic(topology);
+    // create a test driver. we will use this to pipe data to our topology
+    testDriver = Utils.createTestDriverAndTestTopic(topology);
 
-		// create the test input topic
-		inputTopic = testDriver.createInputTopic("users", Serdes.Void().serializer(), Serdes.String().serializer());
-	}
+    // create the test input topic
+    inputTopic =
+        testDriver.createInputTopic(
+            "users", Serdes.Void().serializer(), Serdes.String().serializer());
+  }
 
-	@Test
-	public void testMain() {
-		// stimulate to use topic named "users"
-		inputTopic.pipeInput("Robert");
-		assertThat(outContent.toString()).isEqualTo("(Processor API) Hello, Robert" + System.lineSeparator());
-	}
+  @Test
+  public void testMain() {
+    // stimulate to use topic named "users"
+    inputTopic.pipeInput("Robert");
+    assertThat(outContent.toString())
+        .isEqualTo("(Processor API) Hello, Robert" + System.lineSeparator());
+  }
 
-	@AfterEach
-	public void restoreStreams() {
-		System.setOut(originalOut);
-		System.setErr(originalErr);
-	}
+  @AfterEach
+  public void restoreStreams() {
+    System.setOut(originalOut);
+    System.setErr(originalErr);
+  }
 }
--- chapter-02/hello-streams/src/test/java/com/example/Utils.java
+++ chapter-02/hello-streams/src/test/java/com/example/Utils.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import java.util.Properties;
-
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
@@ -9,30 +8,30 @@ import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.TopologyTestDriver;
 
 public class Utils {
-	
-	/***
-	 *   set the required properties for running Kafka Streams
-	 * **/
-	static Properties createPros() {
-		Properties config = new Properties();
-		config.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
-		config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
-		config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-		config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Void().getClass());
-		config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-		return config;
-	}
-	
-	/****
-	 *  create a test driver. we will use this to pipe data to our topology
-	 * **/
-	static TopologyTestDriver createTestDriverAndTestTopic(final Topology topology) {
-		// set the required properties for running Kafka Streams
-	    Properties config =  createPros();		
-		
-		// create a test driver. we will use this to pipe data to our topology
-	    TopologyTestDriver testDriver = new TopologyTestDriver(topology, config);
-	    
-	    return testDriver;
-	}
+
+  /***
+   *   set the required properties for running Kafka Streams
+   * **/
+  static Properties createPros() {
+    Properties config = new Properties();
+    config.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
+    config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
+    config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Void().getClass());
+    config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    return config;
+  }
+
+  /****
+   *  create a test driver. we will use this to pipe data to our topology
+   * **/
+  static TopologyTestDriver createTestDriverAndTestTopic(final Topology topology) {
+    // set the required properties for running Kafka Streams
+    Properties config = createPros();
+
+    // create a test driver. we will use this to pipe data to our topology
+    TopologyTestDriver testDriver = new TopologyTestDriver(topology, config);
+
+    return testDriver;
+  }
 }
